2a257975c856718b56e7e43542099128eb6137f9
farazi <farazi@Shumanas-MacBook-Pro.local>
2016-01-13 11:44:15 +0600

    refining

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-	'secret': 'ilovescotchyscotch',
-	'database': 'mongodb://noder:noderauth@proximus.modulusmongo.net:27017/so9pojyN'
+    'secret': 'nurnurnurnurnur',
+    'database': process.env.MONGODBURL || 'mongodb://localhost/mgmbeckend'
 
 };
