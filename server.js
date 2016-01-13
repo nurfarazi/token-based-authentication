ae297d28a595cef3586e9feaa5c9ac140abc3caf
farazi <farazi@Shumanas-MacBook-Pro.local>
2016-01-13 11:27:51 +0600

    refining

--- server.js
+++ server.js
@@ -4,15 +4,15 @@ var bodyParser = require('body-parser');
 var morgan = require('morgan');
 var mongoose = require('mongoose');
 
-var jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
-var config = require('./config'); // get our config file
-var User = require('./app/models/user'); // get our mongoose model
+var jwt = require('jsonwebtoken');
+var config = require('./config');
+var User = require('./app/models/user');
 
 // =================================================================
 // configuration ===================================================
 // =================================================================
-var port = process.env.PORT || 8180; // used to create, sign, and verify tokens
-mongoose.connect(config.database); // connect to database
+var port = process.env.PORT || 8180;
+mongoose.connect(config.database);
 app.set('superSecret', config.secret); // secret variable
 
 // use body parser so we can get info from POST and/or URL parameters
